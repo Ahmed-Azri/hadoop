2dafaa3d7529e75d909ce62041f32bc67172478c
z956 <z789512346@gmail.com>
2014-03-09 21:58:11 +0800

    modify the total num of reducer

--- MR_Job/src/WordCount.java
+++ MR_Job/src/WordCount.java
@@ -60,7 +60,7 @@ public class WordCount {
 
     job.setMapperClass(TokenizerMapper.class);
     job.setReducerClass(IntSumReducer.class);
-	job.setNumReduceTasks(16);
+	job.setNumReduceTasks(8);
 	job.setReduceSpeculativeExecution(false);
 
     job.setOutputKeyClass(Text.class);
--- run_wordcount.sh
+++ run_wordcount.sh
@@ -3,4 +3,4 @@ if [ $# -ne 1 ]; then
 	exit 0
 fi
 
-bin/hadoop jar WordCount.jar org.myorg.WordCount /hduser/data_3/* $1
+bin/hadoop jar WordCount.jar org.myorg.WordCount /hduser/data_30/* $1
